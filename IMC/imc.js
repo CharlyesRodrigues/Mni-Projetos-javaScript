e7d67072d5ec4688f7bff4ce07e07c4d3c74c28d
CharlyesRodrigues <charlyessouza@gmail.com>
2023-02-21 10:00:36 -0300

    Atualizado

--- IMC/imc.js
+++ IMC/imc.js
@@ -1,20 +1,47 @@
 const calcular = document.getElementById('calcular');
 
 
-function imc (){
+function imc() {
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
-    
-if (nome === "" | altura === "" | peso ===""){
 
-alert ( "Por favor preecha todos os campos");
-}else{
+    const valorImc = (peso / (altura * altura)).toFixed(1);
 
-resultado.textContent = 'Preenchidos'
-    
-}
+    if (nome === "" | altura === "" | peso === "") {
+
+        alert("Por favor preecha todos os campos");
+    } else {
+
+
+        let classificacao = "";
+
+        if (valorImc < 18.5) {
+
+            classificacao = 'Abaixo do peso';
+            
+        } else if (valorImc < 25) {
+
+            classificacao = ' Com peso Ideal.Parabéns !!!';
+        } else if (valorImc < 30) {
+
+            classificacao = 'Levemente acima do peso';
+          
+        } else if (valorImc < 35) {
+
+            classificacao = 'com Obsesidade grau I';
+        } else if (valorImc < 40) {
+
+            classificacao = 'com Obsesidade grau II';
+        } else {
+
+            classificacao = 'com Obsesidade grau III. Cuidado !!!';
+        }
+
+        resultado.textContent = `${nome} o seu IMC  é ${valorImc} e vc esta ${classificacao}`
+
+    }
 
 
 }
