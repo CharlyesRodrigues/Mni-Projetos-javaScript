bfc628d800c2ae1dead8f31dbaff854a27552756
CharlyesRodrigues <charlyessouza@gmail.com>
2023-02-21 20:20:47 -0300

    Update semaforo.js

--- SEMAFORO/semaforo.js
+++ SEMAFORO/semaforo.js
@@ -4,11 +4,19 @@ const buttons = document.getElementById('buttons');
 
 
 const trafficLght =(event) =>{
-console.log(event.target.id);
 
+turnOn[event.target.id]();
 
 }
 
+/*  Criando um objeto com  arrow function*/
+const turnOn = {
+
+    'red' : ()=> img.src="./images/vermelho.png",
+    'yellow' : ()=>img.src="./images/amarelo.png",
+    'green' : ()=>img.src="./images/verde.png"
+
+}
 
 
 buttons.addEventListener ('click', trafficLght);
