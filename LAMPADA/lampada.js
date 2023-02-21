d108e8a76729779fc39bb71da97d3cdb94137c18
CharlyesRodrigues <charlyessouza@gmail.com>
2023-02-21 14:38:51 -0300

    Update lampada.js

--- LAMPADA/lampada.js
+++ LAMPADA/lampada.js
@@ -4,19 +4,38 @@ const desligando = document.getElementById('desligar');
 const quebrando = document.getElementById('lampada');
 
 
-function ligar(){
 
-   lampada.src = "./images/ligada.jpg";
-   lampada.alt = "lâmpada ligada";
+function isLampBroken() {
+
+    return lampada.src.indexOf('quebrada') > - 1
 }
-function desligar(){
-    lampada.src = "./images/desligada.jpg";
-    lampada.alt = "lâmpada desligada";
+
+function ligar() {
+    if (!isLampBroken()) {
+        lampada.src = "./images/ligada.jpg";
+        lampada.alt = "lâmpada ligada";
+    }
+}
+
+function desligar() {
+
+    if (!isLampBroken()) {
+        lampada.src = "./images/desligada.jpg";
+        lampada.alt = "lâmpada desligada";
+    }
+}
+
+function quebrar() {
+    lampada.src = "./images/quebrada.jpg";
+    lampada.alt = "lâmpada quebrada";
+
 }
 
 
 ligando.addEventListener('click', ligar);
-desligando.addEventListener('click',desligar);
+desligando.addEventListener('click', desligar);
 
 lampada.addEventListener('mouseover', ligar);
 lampada.addEventListener('mouseleave', desligar);
+
+quebrando.addEventListener('dblclick', quebrar);
