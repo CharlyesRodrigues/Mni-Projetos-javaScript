99d689fb43b4d03dc024334dbb7b59af24b0288f
CharlyesRodrigues <charlyessouza@gmail.com>
2023-02-21 20:09:02 -0300

    Atualizado

--- SEMAFORO/semaforo.js
+++ SEMAFORO/semaforo.js
@@ -1,21 +1,14 @@
-const verde = document.getElementById('verde');
-const amarelo = document.getElementById('amarelo');
-const vermelho = document.getElementById('vermelho');
-const automatico = document.getElementById('automatico');
-const picture = document.getElementById('imagem');
+
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttons');
 
 
-function esverdear(){
-picture.src='./images/verde.png';
-}
-function amarelar(){
-picture.src='./images/amarelo.png';
-}
-function avermelhar(){
-picture.src = './images/vermelho.png';
+const trafficLght =(event) =>{
+console.log(event.target.id);
+
+
 }
 
 
-verde.addEventListener('click', esverdear);
-vermelho.addEventListener ('click' , avermelhar);
-amarelo.addEventListener ('click', amarelar)
+
+buttons.addEventListener ('click', trafficLght);
