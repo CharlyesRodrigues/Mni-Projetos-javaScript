efd7b17fb1aa22717ba771be3a4431c1e666ab2c
CharlyesRodrigues <charlyessouza@gmail.com>
2023-02-21 21:21:30 -0300

    Update semaforo.js

--- SEMAFORO/semaforo.js
+++ SEMAFORO/semaforo.js
@@ -1,7 +1,7 @@
 
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
-
+let colorIndex = 0; 
 
 const trafficLight =(event) =>{
 
@@ -14,9 +14,44 @@ const turnOn = {
 
     'red':    ()=> img.src="./images/vermelho.png",
     'yellow': ()=>img.src="./images/amarelo.png",
-    'green':  ()=>img.src="./images/verde.png"
+    'green':  ()=>img.src="./images/verde.png",
+    'automatic': () => setInterval(changeColor, 1000)
 
 }
+
+const changeColor = ()=>{
+
+    const colors = ['red', 'yellow', 'green'];
+    const color = colors[colorIndex];
+    turnOn[color]();
+    nextIndex();
+    
+    }
+
+const nextIndex =()=>{
+/**
+ * 
+ * Exemplo de ternário com a mesma funcionalidade do IF a seguir
+ * 
+ *  colorIndex = colorIndex < 2 ? ++ colorIndex:0; 
+ * 
+ */
+
+    if (colorIndex < 2){
+
+    colorIndex ++
+
+}else {
+
+    colorIndex = 0;
+}
+
+
+}
+
+
+
+
 
 
 buttons.addEventListener ('click', trafficLight);
