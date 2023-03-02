b53399be46dedd9b40bccdea27210426e9bb191c
CharlyesRodrigues <charlyessouza@gmail.com>
2023-03-02 16:15:50 -0300

    Update index.js

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -67,3 +67,32 @@ const selecionarOperador = (evento) => {
     }
 }
 operadores.forEach(operador => operador.addEventListener('click', selecionarOperador));
+
+const ativarIgual =()=>{
+calcular();
+operador = undefined;
+
+}
+
+document.getElementById('igual').addEventListener('click', ativarIgual)
+
+
+
+const  limparDisplay =()=>  display.textContent =''; 
+
+document.getElementById('limparDisplay').addEventListener('click', limparDisplay);
+
+const limparCalculo = () => {
+limparDisplay();
+operador = undefined ;
+novoNumero = true;
+numeroAnterior = undefined ;   
+
+}
+
+document.getElementById('limparCalculo').addEventListener('click', limparCalculo);
+
+const removerUltimoNumero = () => display.textContent = display.textContent.slice( 0 ,-1);
+
+
+document.getElementById('backspace').addEventListener('click',removerUltimoNumero );
